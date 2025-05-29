d7d95930ae426bcb03895a2e599e6bd8d41fdddb
paqsa <gigapaqsa69@gmail.com>
2025-05-29 12:20:53 +0400

    updates

--- src/components/defiDashboard/defi.jsx
+++ src/components/defiDashboard/defi.jsx
@@ -1,9 +1,13 @@
 import "./defi.css";
 
+
 export default function defiDashboard() {
+    
     return (
         <div className="defiDashboardContent">
             <p>Defi Dashboard</p>
+            
         </div>
+        
     )
 }
