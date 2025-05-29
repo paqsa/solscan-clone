6aeca55ef1fd389b31aae0fc84429887d5a9c6ba
paqsa <gigapaqsa69@gmail.com>
2025-05-29 14:03:09 +0400

    volume update

--- src/components/defiDashboard/volumeinfo.jsx
+++ src/components/defiDashboard/volumeinfo.jsx
@@ -1,16 +1,25 @@
 import "./volume.css"
+import JupiterPhoto from "./../../assets/volume/jupiter.png";
+import MeteoraPhoto from "./../../assets/volume/meteora.png";
+import OrcaPhoto from "./../../assets/volume/orca.png";
+import PumpfunPhoto from "./../../assets/volume/pumpfun.png";
+import RaydiumPhoto from "./../../assets/volume/raydium.png";
 
 export default function volumeInfo() {
     return(
         <div className="volumeInfo">
             <div className="volumeContent">
 
-                <div>
-                    <p>AMM</p>
+                <div className="volumeInfoStart">
+                    <p className="AMM">AMM</p>
 
-                    <p>Volume 24H</p>
+                    <p className="volume24H">Volume 24H</p>
 
                     <p>Total Txs 24H</p>
+                </div>
+
+                <div>
+
                 </div>
             </div>
         </div>
