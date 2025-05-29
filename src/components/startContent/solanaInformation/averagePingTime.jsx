17b2cf5dc79f33ab8b21e50720bec9e99edc02c8
paqsa <gigapaqsa69@gmail.com>
2025-05-29 17:01:44 +0400

    TPS

---
+++ src/components/startContent/solanaInformation/averagePingTime.jsx
@@ -0,0 +1,10 @@
+import AveragePingTime from "../../../components/averagePing/pingtime";
+import "./solanainfo"
+
+export default function averagePingTime () {
+    return (
+        <div className="trueTpsInfo">
+            <AveragePingTime></AveragePingTime>
+        </div>
+    )
+}
