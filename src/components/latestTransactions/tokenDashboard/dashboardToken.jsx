757a75f7dc6a75ad7996c3423e66c0db24439194
paqsa <gigapaqsa69@gmail.com>
2025-05-29 14:24:29 +0400

    token update

--- src/components/latestTransactions/tokenDashboard/dashboardToken.jsx
+++ src/components/latestTransactions/tokenDashboard/dashboardToken.jsx
@@ -2,8 +2,18 @@ import "./tokenDash.css";
 
 export default function tokenDashboard() {
     return (
-        <div>
-            
+        <div className="tokenDashboard">
+            <div className="tokenContent">
+
+                <p>Token Dashboard</p>
+
+                <div>
+                    <p>Token</p>
+                    <p>Symbol</p>
+                    <p>Price</p>
+                    <p>Market Cap (F.D)</p>
+                </div>
+            </div>
         </div>
     )
 }
