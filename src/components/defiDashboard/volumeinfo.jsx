86e654680332b936402387a40198171d4b4d3d4a
paqsa <gigapaqsa69@gmail.com>
2025-05-29 13:24:10 +0400

    volume update

--- src/components/defiDashboard/volumeinfo.jsx
+++ src/components/defiDashboard/volumeinfo.jsx
@@ -1,1 +1,18 @@
 import "./volume.css"
+
+export default function volumeInfo() {
+    return(
+        <div className="volumeInfo">
+            <div className="volumeContent">
+
+                <div>
+                    <p>AMM</p>
+
+                    <p>Volume 24H</p>
+
+                    <p>Total Txs 24H</p>
+                </div>
+            </div>
+        </div>
+    )
+}
