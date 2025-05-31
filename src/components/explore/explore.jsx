c0e576be69551ad2aa1a5916d569d33e7f7e2da0
paqsa <gigapaqsa69@gmail.com>
2025-05-31 15:45:31 +0400

    update explore, topmarkets

--- src/components/explore/explore.jsx
+++ src/components/explore/explore.jsx
@@ -5,7 +5,7 @@ export default function explore() {
         <div className="exploreContent">
             <h1>Explore Solana Blockchain</h1>
             <input className="inputExplore" type="text" placeholder="Search Transaction, blocks, programs and tokens"/>
-            <p>Sponsored: Best Wallet: NO KYC Crypto Wallet with FREE Airdrop! Explore More.</p>
+            <p className="sponsoredStyle">Sponsored: Best Wallet: NO KYC Crypto Wallet with FREE Airdrop! <span className="exploreMore">Explore More.</span></p>
         </div>
     )
 }
